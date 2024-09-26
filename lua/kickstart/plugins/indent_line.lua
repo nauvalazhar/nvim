699dcd273dd0c04a6258af5893af39857e729aec
Nauval <nauvalazhar2@gmail.com>
2024-09-26 14:26:21 +0700

    fucking light config let's goooo

---
+++ lua/kickstart/plugins/indent_line.lua
@@ -0,0 +1,17 @@
+return {
+  { -- Add indentation guides even on blank lines
+    'lukas-reineke/indent-blankline.nvim',
+    -- Enable `lukas-reineke/indent-blankline.nvim`
+    -- See `:help ibl`
+    main = 'ibl',
+    opts = {
+      indent = {
+        char = '▏',
+        tab_char = '▏',
+      },
+      scope = {
+        enabled = false,
+      },
+    },
+  },
+}
